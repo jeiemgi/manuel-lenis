730bbd7523f73c18c1591d27ed41d336158d90df
José Gutiérrez <hello@josegtz.com>
2024-10-14 16:16:22 -0600

    add cursor

--- src/scripts/Cursor.ts
+++ src/scripts/Cursor.ts
@@ -2,16 +2,36 @@ class Cursor {
   x: number = 0;
   y: number = 0;
   cursorEl: HTMLDivElement;
+  cursorImg: HTMLImageElement;
+  hoverEls: HTMLElement[] = [];
 
   constructor(selector: string) {
     const cursorEl = document.querySelector<HTMLDivElement>(selector);
+    const cursorImg = cursorEl?.querySelector("img");
+
     if (!cursorEl) throw new Error("Cursor element not found");
+    if (!cursorImg) throw new Error("Cursor Image not found");
 
     this.cursorEl = cursorEl;
-    console.log("cursor created");
+    this.cursorImg = cursorImg;
+
     document.querySelector("body")?.classList.add("custom-cursor");
     document.addEventListener("mousemove", this.onMouseMove);
+
+    const hoverEls = document.querySelectorAll<HTMLElement>("[data-hover]");
+    hoverEls.forEach((el: HTMLElement) => {
+      el.addEventListener("mouseover", this.onHover);
+      el.addEventListener("mouseout", this.onHoverOut);
+    });
   }
+
+  onHover = (e: MouseEvent) => {
+    this.cursorEl.classList.add("hovering");
+  };
+
+  onHoverOut = (e: MouseEvent) => {
+    this.cursorEl.classList.remove("hovering");
+  };
 
   onMouseMove = (e: MouseEvent) => {
     this.x = e.clientX;
@@ -23,6 +43,10 @@ class Cursor {
 
   destroy() {
     document.removeEventListener("mousemove", this.onMouseMove);
+    this.hoverEls.forEach((el: HTMLElement) => {
+      el.removeEventListener("mouseover", this.onHover);
+      el.removeEventListener("mouseout", this.onHoverOut);
+    });
   }
 }
 
