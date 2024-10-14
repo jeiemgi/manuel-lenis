d1a69afe9f2ea195b36f3b2cc9443366b2069eb9
José Gutiérrez <hello@josegtz.com>
2024-10-14 11:14:04 -0600

    fixing structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import icon from 'astro-icon';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+    integrations: [icon(), tailwind({
+        applyBaseStyles: false,
+    })]
 });
